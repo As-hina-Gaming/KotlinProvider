72b4040bf883fc2232160ce39b1060a30281f399
Motzkiste <motzkiste@outlook.com>
2022-06-12 20:38:24 +0200

    rename and create simple plugin logic

---
+++ src/main/kotlin/net/eratiem/kotlinprovider/KotlinProviderPlugin.kt
@@ -0,0 +1,14 @@
+package net.eratiem.kotlinprovider
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinProviderPlugin: JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("Kotlin can now be used!")
+    }
+
+    override fun onDisable() {
+        logger.info("Kotlin is no longer useable!")
+    }
+}
